44cbd12cbc32fc8540d2a9ab681c0ebb7b34c5e1
Rodolfo Roman <rromanv@gmail.com>
2020-11-12 01:17:03 -0600

    Change  Colors

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,36 @@
   <div class="min-h-screen flex flex-col justify-center items-center">
     <header>
       <div class="prose-lg">
-        <h1 class="">🚀 My Todo App With Tailwind and Vue3 🚀</h1>
+        <h1 class="text-green-700">🚀 My Todo App With Tailwind and Vue3 🚀</h1>
         <form class="flex" @submit.prevent="addTodo">
           <input type="text" v-model="newTodo" class="flex-grow" />
           <button
             type="submit"
-            class="bg-green-500 text-white px-4 ml-2 rounded-md"
+            class="bg-green-700 hover:bg-green-500 text-white px-4 ml-2 rounded-md"
           >
             Add Todo
           </button>
         </form>
       </div>
     </header>
-    <main class="prose">
-      <div class="pendings">
-        <h2>Pending Todos - {{ pendingTodos.length }}</h2>
+    <main>
+      <div class="pendings mt-12">
+        <h2 class="text-4xl text-bold text-green-500">
+          Pending Todos - {{ pendingTodos.length }}
+        </h2>
         <div
           v-for="t in pendingTodos"
           :key="t.id"
           @click="changeStatus(t.id, 'completed')"
-          class="text-center p-3 bg-white mt-4 rounded shadow-lg cursor-pointer hover:bg-green-500"
+          class="text-center p-3 bg-white mt-4 text-green-500 rounded shadow-lg cursor-pointer hover:bg-green-500 hover:text-white"
         >
           {{ t.todo }}
         </div>
       </div>
-      <div class="completed">
-        <h2>Completed Todos - {{ completedTodos.length }}</h2>
+      <div class="completed mt-12">
+        <h2 class="text-4xl text-bold text-red-500">
+          Completed Todos - {{ completedTodos.length }}
+        </h2>
         <div
           v-for="t in completedTodos"
           :key="t.id"
